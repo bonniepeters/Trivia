610a3b32e66156a286faa918a4dca0117b999e9d
bonniepeters <bonnie.e.peters@gmail.com>
2019-09-16 11:21:09 -0600

    Question class created

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+// class to follow for each question
+class Question {
+    constructor(questionNumber, questionAsked, answerChoices) {
+    this.questionNumber = questionNumber
+    this.questionAsked = questionAsked;
+    this.answerChoices = answerChoices;
+    }
+}
+
+// function to create questions
+
+// Test creating single question
+let question1 = new Question(1,`What is Geoffrey the Butler's last name?`, [{answer: 'Smith', check: false}, {answer: 'Banks', check: false}, {answer: 'Williams', check: false}, {answer: 'Butler', check: true}]);
+
+console.log(question1);
+//append questions to quiz
