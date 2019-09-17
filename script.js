237d0b61a040661c73644ed96f53618bae9f80d3
bonniepeters <bonnie.e.peters@gmail.com>
2019-09-17 11:25:09 -0600

    Finished game functions and added background image

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+const game = document.querySelector(".game");
 const quiz = document.querySelector(".quiz");
-const modal = document.getElementById("modal");
+const start = document.querySelector(".start");
+const end = document.getElementById("end");
 const reset = document.getElementById("reset");
 const scoreBoard = document.querySelector(".score");
+const questionNumber = document.querySelector(".question-number");
+const results = document.querySelector(".results");
+const instructions = document.querySelector(".instructions");
+let currentQuestionNumber = 0;
 let currentQuestionIndex = 0;
 let score = 0;
 let currentQuestion = '';
@@ -30,7 +36,14 @@ const questions = [
     new Question(`When Will is kicked out of his apartment, where does he end up living?`, [`West Philadelphia`, `Down the street`, `Hollywood`, `The pool house`], `The pool house`)
 ];
 
-nextQuestion();
+function startGame() {
+    game.style.display = 'block';
+    instructions.style.display = 'none';
+    start.style.display = 'none';
+    nextQuestion();
+}
+
+start.addEventListener("click", startGame);
 
 // Render/append a question incrementally as the user selects "load next question"
 function renderQuestion() {
@@ -62,6 +75,8 @@ function nextQuestion() {
     clearCurrentQuestion();
     renderQuestion();
     currentQuestionIndex++;
+    currentQuestionNumber++;
+    questionNumber.innerHTML = `Question ${currentQuestionNumber}`;
 };
 
 function clearCurrentQuestion() {
@@ -87,16 +102,28 @@ function checkAnswer(evt) {
 };
 
 function endGame() {
-    modal.style.display = 'block';
+    end.style.display = 'block';
+    renderResults();
 };
 
 function resetGame() {
-    modal.style.display = 'none';
+    end.style.display = 'none';
     currentQuestionIndex = 0;
     currentQuestion = '';
+    currentQuestionNumber = 0;
     score = 0;
     scoreBoard.innerHTML = `Score: ${score}/10`;
     nextQuestion();
 };
+
+function renderResults() {
+    if (score < 5) {
+        results.innerHTML = `You got ${10-score} out of 10 wrong. Looks like you're up to no good.`;
+    } else if (score < 8) {
+        results.innerHTML = `You got ${score} out of 10 right. Not too bad, but you might end up sleeping in the pool house. Yo, homes smell ya later!`;
+    } else {
+        results.innerHTML = `You got ${score} out of 10 right. Look at you chillin' out maxin' relaxin' all cool!`;
+    }
+}
 
 reset.addEventListener('click', resetGame);
