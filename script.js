ee14e8f84bde6e38e8d074cc604628f06064b955
bonniepeters <bonnie.e.peters@gmail.com>
2019-09-16 23:34:18 -0600

    New approach to rendering questions

--- script.js
+++ script.js
@@ -1,56 +1,57 @@
 const quiz = document.querySelector(".quiz");
-const scoreBoard = document.querySelector(".scroe");
-let score = 0;
+// const scoreBoard = document.querySelector(".score");
+// let score = 0;
+let currentQuestionIndex = 0;
+const next = document.querySelector("#next");
+let currentQuestion = '';
 
 // class to follow for each question
 class Question {
-    constructor(questionAsked, answerChoices) {
+    constructor(questionAsked, choices, answer) {
     this.questionAsked = questionAsked;
-    this.answerChoices = answerChoices;
-    }
-    userChoice(choice) {
-        if (choice.check = true) {
-            score++
-        }
+    this.choices = choices;
+    this.answer = answer;
     }
 };
+
 // Array of questions and their answers
-    // Each question should have an array of 4 possible answers with one having a check boolean set to true
+// Each question has an array of 4 choices with one indicated as the answer
 const questions = [
-    new Question(`What is Geoffrey the Butler's last name?`, [{answer: `Smith`, check: false}, {answer: `Banks`, check: false}, {answer: `Williams`, check: false}, {answer: `Butler`, check: true}]),
-    new Question(`What song does Carlton do the iconic "Carlton Dance" to?`, [{answer: `It's Not Unusual`, check: true}, {answer: `Jump On It`, check: false}, {answer: `She's a Lady`, check: false}, {answer: `Gettin' Jiggy Wit It`, check: false}]),
-    new Question(`What college do Will and Carlton attend?`, [{answer: `UCLA`, check: false}, {answer: `ULA`, check: true}, {answer: `USC`, check: false}, {answer: `Cal Poly`, check: false}]),
-    new Question(`When Will is kicked out of his apartment, where does he end up living?`, [{answer: `West Philadelphia`, check: false}, {answer: `Down the street from his aunt and uncle`, check: false}, {answer: `Hollywood`, check: false}, {answer: `The pool house`, check: true}]),
-    new Question(`What food is served where Ashley works in the mall?`, [{answer: `Corn dogs`, check: true}, {answer: `Pizza`, check: false}, {answer: `Ice cream`, check: false}, {answer: `Tacos`, check: false}]),
-    new Question(`Which of these is NOT one of Aunt Vivian's sisters' names?`, [{answer: `Vy`, check: false}, {answer: `Helen`, check: false}, {answer: `Janice`, check: false}, {answer: `Shawna`, check: true}]),
-    new Question(`What music group inspired Nicky's name?`, [{answer: `Rob Base and DJ E-Z Rock`, check: false}, {answer: `Earth, Wind, and Fire`, check: false}, {answer: `Boyz II Men`, check: true}, {answer: `Bell Biv DeVoe`, check: false}]),
-    new Question(`What was Hilary's job before getting her talk show?`, [{answer: `Singer`, check: false}, {answer: `Waitress`, check: false}, {answer: `Weather Girl`, check: true}, {answer: `Dancer`, check: false}]),
-    new Question(`Where did Uncle Phil propose to Aunt Vivian?`, [{answer: `Soul Train`, check: true}, {answer: `Red Lobster`, check: false}, {answer: `In the car`, check: true}, {answer: `In a courthouse`, check: false}]),
-    new Question(`What new career does Uncle Phil go into?`, [{answer: `Judge`, check: true}, {answer: `Priest`, check: false}, {answer: `Scientist`, check: true}, {answer: `Corrections Officer`, check: false}]),
+    new Question(`What is Geoffrey the Butler's last name?`, [`Smith`, `Banks`, `Williams`, `Butler`], `Butler`),
+    new Question(`What song does Carlton do the iconic "Carlton Dance" to?`, [`It's Not Unusual`, `Jump On It`, `She's a Lady`, `Gettin' Jiggy Wit It`], `It's Not Unusual`),
+    new Question(`What college do Will and Carlton attend?`, [`UCLA`, `ULA`, `USC`, `Cal Poly`], `ULA`),
+    new Question(`What food is served where Ashley works in the mall?`, [`Corn dogs`, `Pizza`, `Ice cream`, `Tacos`], `Corn dogs`),
+    new Question(`Which of these is NOT one of Aunt Vivian's sisters' names?`, [`Vy`, `Helen`, `Janice`, `Shawna`], `Shawna`),
+    new Question(`What music group inspired Nicky's name?`, [`Rob Base and DJ E-Z Rock`, `Earth, Wind, and Fire`, `Boyz II Men`, `Bell Biv DeVoe`], `Boyz II Men`),
+    new Question(`What was Hilary's job before getting her talk show?`, [`Singer`, `Waitress`, `Weather Girl`, `Dancer`], `Weather Girl`),
+    new Question(`Where did Uncle Phil propose to Aunt Vivian?`, [`Soul Train`, `Red Lobster`, `In the car`, `In a courthouse`], `Soul Train`),
+    new Question(`What new career does Uncle Phil go into?`, [`Judge`, `Priest`, `Scientist`, `Corrections Officer`], `Judge`),
+    new Question(`When Will is kicked out of his apartment, where does he end up living?`, [`West Philadelphia`, `Down the street`, `Hollywood`, `The pool house`], `The pool house`)
 ];
-console.log(questions);
-
-// function to create questions
-questions.forEach(function(question) {
-    let quizQuestion = document.createElement("li");
-    let questionAnswerList = document.createElement("div");
-    quizQuestion.className = "quiz questions";
-    quizQuestion.innerText = question.questionAsked;
-    console.log(question);
-    console.log(quizQuestion);
-    questionAnswerList.className = "answers"
-    for (let i = 0; i < 4; i++) {
-        let questionAnswerListItem = document.createElement("input");
-        questionAnswerListItem.setAttribute("type", "button");
-        questionAnswerListItem.className = "quiz answers";
-        questionAnswerListItem.value = question.answerChoices[i].answer;
-        //append questions and answers to quiz
-        quiz.appendChild(quizQuestion);
-        quizQuestion.appendChild(questionAnswerList);
-        questionAnswerList.appendChild(questionAnswerListItem);
-        };
-});
 
 
-console.log(questions);
-console.log(question);
+function renderQuestion() {
+    if (currentQuestionIndex < questions.length) {
+        currentQuestion = questions[currentQuestionIndex];
+        let renderedQuestion = document.createElement("li");
+        renderedQuestion.innerHTML = currentQuestion.questionAsked;
+        quiz.appendChild(renderedQuestion);
+    }
+    renderAnswers();
+};
+
+function renderAnswers() {
+    let eachChoice = questions[currentQuestionIndex].choices;
+    for (let q = 0; q < 4; q++) {
+        choice = document.createElement("button");
+        choice.innerHTML = eachChoice[q];
+        quiz.appendChild(choice);
+    }
+};
+
+function nextQuestion() {
+    renderQuestion();
+    currentQuestionIndex++;
+};
+
+next.addEventListener("click", nextQuestion);
